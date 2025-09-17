5247a2655c47994e5ec19f030f532ee3daa37dad
Javeth <rojasdiego133@gmail.com>
2025-09-17 14:12:59 -0600

    Pase la funcion del ej1 a el archivo funciones.php

---
+++ practicas/p06/index.php
@@ -0,0 +1,23 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Práctica 4</title>
+</head>
+<body>
+    <?php include 'src/funciones.php'; ?>
+
+    <h2>Ejercicio 1</h2>
+    <p>Escribir programa para comprobar si un número es un múltiplo de 5 y 7</p>
+
+
+    <h2>Ejemplo de POST</h2>
+    <form action="http://localhost/tecweb/practicas/p04/index.php" method="post">
+        Name: <input type="text" name="name"><br>
+        E-mail: <input type="text" name="email"><br>
+        <input type="submit">
+    </form>
+    <br>
+    
+</body>
+</html>
